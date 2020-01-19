83b362e51e582cc7fde516269fc3b3bac2e3de5d
Mario <gabbriel.mario@gmail.com>
2020-01-19 19:27:55 -0300

    v1.0 Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { InfoComponent } from './Component/info/info.component';
+import { NewsComponent } from './Component/news/news.component';
+import { ServicesComponent } from './Component/services/services.component';
+import { ContactComponent } from './Component/contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: '', component: HomeComponent},
+	{path: 'home', component: HomeComponent},
+	{path: 'info', component: InfoComponent},
+	{path: 'news', component: NewsComponent},
+	{path: 'services', component: ServicesComponent},
+	{path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Component/header/header.component';
+import { HomeComponent } from './Component/home/home.component';
+import { InfoComponent } from './Component/info/info.component';
+import { FooterComponent } from './Component/footer/footer.component';
+import { ServicesComponent } from './Component/services/services.component';
+import { NewsComponent } from './Component/news/news.component';
+import { ContactComponent } from './Component/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    InfoComponent,
+    FooterComponent,
+    ServicesComponent,
+    NewsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
